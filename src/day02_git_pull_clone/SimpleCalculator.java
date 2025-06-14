3248ea1110186ac8b80ca4057a564e4041838259
niloufar23 <n.sarvari97@gmail.com>
2025-06-14 17:36:30 +0200

    Update SimpleCalculator.java

--- src/day02_git_pull_clone/SimpleCalculator.java
+++ src/day02_git_pull_clone/SimpleCalculator.java
@@ -4,6 +4,9 @@ public class SimpleCalculator {
     public static int add(int a, int b) {
         return a + b;
     }
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
 
     public static int subtract(int a, int b) {
         return a - b;
