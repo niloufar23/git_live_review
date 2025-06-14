1f6c553a7b16e0458eed486561e09e6173d4d217
niloufar23 <n.sarvari97@gmail.com>
2025-06-14 17:38:35 +0200

    Update SimpleCalculator.java

--- src/day02_git_pull_clone/SimpleCalculator.java
+++ src/day02_git_pull_clone/SimpleCalculator.java
@@ -12,6 +12,14 @@ public class SimpleCalculator {
         return a - b;
     }
 
+    public static double divide(int a, int b) {
+        if (b == 0) {
+        System.out.println("Cannot divide by zero.");
+        return 0;
+    }
+        return (double) a / b;
+    }
+
     public static void main(String[] args) {
         int x = 10, y = 5;
         System.out.println("Add: " + add(x, y));
